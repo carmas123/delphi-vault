826dcb060fdc9d37b03e75acd8ff4ea089c0d1c2
darian <darian@darianmiller.com>
2012-11-24 19:34:51 +0000

    Source reformat, functionality unchanged

--- Source/DelphiVault.StringUtils.pas
+++ Source/DelphiVault.StringUtils.pas
@@ -1,110 +1,110 @@
-//Written with Delphi XE3 Pro
-//Created Nov 24, 2012 by Darian Miller
+// Written with Delphi XE3 Pro
+// Created Nov 24, 2012 by Darian Miller
 unit DelphiVault.StringUtils;
 
 interface
+
 uses
   System.SysUtils;
 
 
-  function Trim(const s: string; c: Char): string; overload;
-  function Trim(const s: string; const c: array of Char): string; overload;
-  function Trim(const s: string; const c: string): string; overload;
-  function Trim(const s: string; const c: TSysCharSet): string; overload;
+  function Trim(const Source:string; const TrimThis:Char):string; overload;
+  function Trim(const Source:string; const TrimThis: array of Char):string; overload;
+  function Trim(const Source:string; const TrimThis:string):string; overload;
+  function Trim(const Source:string; const TrimThis:TSysCharSet):string; overload;
 
-  function CharInArray(c: Char; ar: array of Char): Boolean;
-  function CharInString(c: Char; s:String): Boolean;
+  function CharInArray(SearchFor:Char; Source: array of Char):Boolean;
+  function CharInString(SearchFor:Char; Source:string):Boolean;
 
 
 implementation
 
-
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function Trim(const s: string; c: Char): string;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function Trim(const Source:string; const TrimThis:Char):string;
 var
-  First, Last: Integer;
+  First, Last:Integer;
 begin
   First := 1;
-  Last := Length(s);
-  while (First <= Last) and (s[First] = c) do
+  Last := Length(Source);
+  while (First <= Last) and (Source[ First ] = TrimThis) do
     Inc(First);
-  while (First < Last) and (s[Last] = c) do
-    Dec(last);
-  Result := Copy(s, First, Last - First + 1);
+  while (First < Last) and (Source[ Last ] = TrimThis) do
+    Dec(Last);
+  Result := Copy(Source, First, Last - First + 1);
 end;
 
 
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function Trim(const s: string; const c: array of Char): string;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function Trim(const Source:string; const TrimThis: array of Char):string;
 var
-  First, Last: Integer;
+  First, Last:Integer;
 begin
   First := 1;
-  Last := Length(s);
-  while (First <= Last) and (CharInArray(s[First],c)) do
+  Last := Length(Source);
+  while (First <= Last) and (CharInArray(Source[ First ], TrimThis)) do
     Inc(First);
-  while (First < Last) and (CharInArray(s[Last],c)) do
-    Dec(last);
-  Result := Copy(s, First, Last - First + 1);
+  while (First < Last) and (CharInArray(Source[ Last ], TrimThis)) do
+    Dec(Last);
+  Result := Copy(Source, First, Last - First + 1);
 end;
 
 
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function Trim(const s: string; const c: string): string;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function Trim(const Source:string; const TrimThis:string):string;
 var
-  First, Last: Integer;
+  First, Last:Integer;
 begin
   First := 1;
-  Last := Length(s);
-  while (First <= Last) and (CharInString(s[First],c)) do
+  Last := Length(Source);
+  while (First <= Last) and (CharInString(Source[ First ], TrimThis)) do
     Inc(First);
-  while (First < Last) and (CharInString(s[Last],c)) do
-    Dec(last);
-  Result := Copy(s, First, Last - First + 1);
+  while (First < Last) and (CharInString(Source[ Last ], TrimThis)) do
+    Dec(Last);
+  Result := Copy(Source, First, Last - First + 1);
 end;
 
 
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function Trim(const s: string; const c: TSysCharSet): string;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function Trim(const Source:string; const TrimThis:TSysCharSet):string;
 var
-  First, Last: Integer;
+  First, Last:Integer;
 begin
   First := 1;
-  Last := Length(s);
-  while (First <= Last) and (CharInSet(s[First], c)) do
+  Last := Length(Source);
+  while (First <= Last) and (CharInSet(Source[ First ], TrimThis)) do
     Inc(First);
-  while (First < Last) and (CharInSet(s[Last], c)) do
-    Dec(last);
-  Result := Copy(s, First, Last - First + 1);
+  while (First < Last) and (CharInSet(Source[ Last ], TrimThis)) do
+    Dec(Last);
+  Result := Copy(Source, First, Last - First + 1);
 end;
 
 
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function CharInArray(c: Char; ar: array of Char): Boolean;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function CharInArray(SearchFor:Char; Source: array of Char):Boolean;
 var
-  i: Integer;
+  i:Integer;
 begin
   Result := True;
-  for i := Low(ar) to High(ar) do
-    if ar[i] = c then
+  for i := low(Source) to high(Source) do
+    if Source[ i ] = SearchFor then
       exit;
   Result := False;
 end;
 
-//Based on answer by Rob Kennedy on Nov 21, 2012 to question:
-//http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
-function CharInString(c: Char; s:String): Boolean;
+// Based on answer by Rob Kennedy on Nov 21, 2012 to question:
+// http://stackoverflow.com/questions/13486438/how-to-trim-any-character-or-a-substring-from-a-string
+function CharInString(SearchFor:Char; Source:string):Boolean;
 var
-  i: Integer;
+  i:Integer;
 begin
   Result := True;
-  for i := Low(s) to High(s) do
-    if s[i] = c then
+  for i := low(Source) to high(Source) do
+    if Source[ i ] = SearchFor then
       exit;
   Result := False;
 end;
